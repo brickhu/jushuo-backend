4b79341469fed4c6b953ce25d830d8b6e30cde9a
Brickhu <brickhu@126.com>
2026-01-29 17:25:54 +0800

    Refactor response message to use MYSQL_ADDRESS from environment variables

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+
 app.get('/', (req, res) => {
-  res.send('欢迎使用微信云托管！-> '+ (process.env.ENV_NAME || "没有"))
+  res.send('欢迎使用微信云托管！-> '+ (MYSQL_ADDRESS || "没有"))
 })
 
 const port = process.env.APP_PORT || 3000
