9fd7a9700077cd6473598198520fbefbaf42deec
Brickhu <brickhu@126.com>
2026-02-01 20:54:55 +0800

    测试部署

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
 
 app.get('/', (req, res) => {
-  res.send('欢迎使用微信云托管！-> '+ (MYSQL_ADDRESS || "没有"))
+  res.send('欢迎光临佳句说！-> '+ (MYSQL_ADDRESS || "没有"))
 })
 
 const port = process.env.APP_PORT || 3000
